e5bb191be64cee20652bcdd201f3d62272ac0b9f
alumno <alumno>
2019-02-05 20:35:01 -0300

    commit6

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,7 +2,10 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
-{
-
+{ var numero = document.getElementById("numeroUno").value;
+  var numero2 = document.getElementById("numeroDos").value;
+  var numeroa = parseInt(numero);
+  var numerob =  parseInt(numero2);
+  alert(numeroa + numerob);
 }
 
