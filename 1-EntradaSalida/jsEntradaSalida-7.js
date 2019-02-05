17281fea5cc6c8f7308c6c92eba907745a11ffb3
alumno <alumno>
2019-02-05 20:55:21 -0300

    commit7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,29 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
-}
+    var numero = parseInt(document.getElementById("numeroUno").value);
+    var numero2 = parseInt(document.getElementById("numeroDos").value);
+    alert(numero + numero2);
+}  
 
 function restar()
 {
-	
+	var numero = parseInt(document.getElementById("numeroUno").value);
+    var numero2 = parseInt(document.getElementById("numeroDos").value);
+    alert(numero - numero2);
 }
 
 function multiplicar()
 { 
-	
+	var numero = parseInt(document.getElementById("numeroUno").value);
+    var numero2 = parseInt(document.getElementById("numeroDos").value);
+    alert(numero * numero2);
 }
 
 function dividir()
 {
-	
+	var numero = parseInt(document.getElementById("numeroUno").value);
+    var numero2 = parseInt(document.getElementById("numeroDos").value);
+    alert(numero / numero2);
 }
 
