5d940eefff18bfe6203eec7e743f3f7c71037f1b
alumno <alumno>
2019-02-05 21:40:31 -0300

    commit9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,7 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
-	
+    var sueldoId = parseInt(document.getElementById("sueldo").value);
+    var aumento = sueldoId + (10 * sueldoId) / 100;
+    var resultado = document.getElementById("resultado").value = aumento
 }
