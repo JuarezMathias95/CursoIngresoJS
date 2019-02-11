2ac59409f9f8cc849865d4ea917bbf567bad7c49
alumno <alumno>
2019-02-11 19:31:59 -0300

    if6

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,19 @@
 function mostrar()
 {
-//tomo la edad  
-
-
-
-
-}//FIN DE LA FUNCIÓN
+      var edadId = parseInt(document.getElementById("edad").value);
+        if(edadId >=18)
+        { 
+          alert("usted es mayor de edad");
+         }
+     else 
+    {
+        if(edadId >= 13 && edadId <= 17)
+    {
+        alert("usted es un adolescente");
+    } 
+    else
+    {
+        alert("usted es un niño");   
+    }
+    }
+}
